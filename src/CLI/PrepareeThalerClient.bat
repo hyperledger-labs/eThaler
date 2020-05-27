41b5612d8aaf03287b44519e592113835a14611a
U-JAGAWORKSTATION\jaga <jaga@swapshub.com>
2020-05-27 13:46:39 -0400

    Initial commit for CLI
    Signed-off-by: U-JAGAWORKSTATION\jaga <jaga@swapshub.com>

---
+++ src/CLI/PrepareeThalerClient.bat
@@ -0,0 +1,9 @@
+@echo off
+REM Copyright 2020 Swapshub
+REM Licensed under the Apache License, Version 2.0 (the "License");
+REM you may not use this file except in compliance with the License.
+REM You may obtain a copy of the License at
+REM http://www.apache.org/licenses/LICENSE-2.0
+
+
+gradlew build copyJars createeThalerClientScript
